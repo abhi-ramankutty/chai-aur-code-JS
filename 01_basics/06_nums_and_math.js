f6ea7119cd825b296f8bd74a44ae566bd20ef838
abhi-ramankutty <59701101+abhi-ramankutty@users.noreply.github.com>
2024-03-07 12:51:19 +0000

    basics of numbers n math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+// const score = 400;
+// const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// console.log('-------------------');
+
+// const newNum = 121.952;
+// console.log(newNum.toPrecision(3));
+// console.log(newNum.toPrecision(4));
+// console.log(newNum.toPrecision(5));
+
+// console.log('-------------------');
+
+// const num  = 1000000;
+// console.log(num.toLocaleString('en-IN'));
+
+// ********************* MATHS **********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = -10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
