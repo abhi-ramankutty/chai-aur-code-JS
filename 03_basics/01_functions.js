4af591b7c91d0d7de600465b53a267f99719701e
abhi-ramankutty <59701101+abhi-ramankutty@users.noreply.github.com>
2024-03-10 05:37:28 +0000

    basics of function v2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,5 +14,32 @@ function loginUserMsg(username = "unknown user") {
 	}
 	return `${username} just logged in`;
 }
+console.log(loginUserMsg("Abhilash"));
 
-console.log(loginUserMsg('Abhilash'))
+console.log("-------------------------");
+
+function calcCartPrice(val1, val2, ...num) {
+	return num;
+}
+console.log(calcCartPrice(100, 200, 300, 400));
+
+const user = {
+	name: "Abhi",
+	prices: 100,
+};
+function handleObj(inputObj) {
+	console.log(`User is ${inputObj.name} and price is ${inputObj.price}`);
+}
+
+// handleObj(user)
+handleObj({ name: "Shinigami", price: 999 });
+
+console.log("-------------------------");
+
+const myArr = [100, 200, 300, 400, 500];
+
+function getSecondVal(inputArr) {
+	return inputArr[1];
+}
+// console.log(getSecondVal(myArr));
+console.log(getSecondVal([1, 2, 3, 4, 5, 6]));
